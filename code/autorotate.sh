64ce82547e0a91d6ef44432a0cf8ca772da8f30c
Sriram V <sriram.vasudevan.93@gmail.com>
2013-07-10 18:02:04 +0530

    Autorate update

--- code/autorotate.sh
+++ code/autorotate.sh
@@ -2,18 +2,25 @@ mkdir -p tempstore
 mkdir -p success
 mkdir -p failed
 
-./deskew "$1" > log.txt
+for f in $1/*
+do
+    ext=${f##*.}
+    if [ "$ext" == "tif" ] || [ "$ext" == "tiff" ] || [ "$ext" == "jpg" ] || [ "$ext" == "jpeg" ] || [ "$ext" == "bmp" ] || [ "$ext" == "png" ] ; then
 
-orientation=$(./detectflip tempstore/temp.tiff 2>&1)
+        ./deskew "$f" > log.txt
 
-if [ "$orientation" == "upside-down" ]; then
-    ./rotateimg tempstore/temp.tiff
-    mv tempstore/temp.tiff success/$(basename $1)
-elif [ "$orientation" == "" ]; then
-    echo "Image size too small. Please try using an image of higher resolution."
-    mv tempstore/temp.tiff failed/$(basename $1)
-else
-    mv tempstore/temp.tiff success/$(basename $1)
-fi
+        orientation=$(./detectflip tempstore/temp.$ext 2>&1)
+
+        if [ "$orientation" == "upside-down" ]; then
+            ./rotateimg tempstore/temp.$ext
+            mv tempstore/temp.$ext success/$(basename $f)
+        elif [ "$orientation" == "" ]; then
+            echo "Image size too small. Please try using an image of higher resolution."
+            cp $f failed/$(basename $f)
+        else
+            mv tempstore/temp.$ext success/$(basename $f)
+        fi
+    fi
+done
 
 rm tempstore -rf
