ea093fec63fd6372dd6f94e851d2784f10f8c162
Sriram V <sriram.vasudevan.93@gmail.com>
2013-07-15 13:36:51 +0530

    Browse added

--- code/autorotate.sh
+++ code/autorotate.sh
@@ -1,39 +1,44 @@
 #/bin/bash
 
-cd ~/autorotate/code/
+rootdir=~/GitHub/autorotate/code/
 
+cd $rootdir
 mkdir -p tempstore
 mkdir -p success
 mkdir -p failed
 
-numfil=$(find $1/ -type f | wc -l)
+numfil=$(find $1/ -type f -maxdepth 1| wc -l)
 echo "${numfil} potential files to analyze"
 id=0
 
 for f in "$1"/*
 do
-    
-    ((id++))
-    echo "${id} out of ${numfil}"
-    ext=${f##*.}
-    if [ "$ext" == "tif" ] || [ "$ext" == "tiff" ] || [ "$ext" == "jpg" ] || [ "$ext" == "jpeg" ] || [ "$ext" == "bmp" ] || [ "$ext" == "png" ] ; then
-        echo "Found an image. Analyzing..."
-        ./deskew "$f" > log.txt
 
-        orientation=$(./detectflip tempstore/temp.$ext 2>&1)
+    if [ -f $f ]
+    then
+        ((id++))
+        echo "${id} out of ${numfil}"
+        ext=${f##*.}
+        if [ "$ext" == "tif" ] || [ "$ext" == "tiff" ] || [ "$ext" == "jpg" ] || [ "$ext" == "jpeg" ] || [ "$ext" == "bmp" ] || [ "$ext" == "png" ] ; then
+            echo "Found an image. Analyzing..."
+            ./deskew "$f" > log.txt
 
-        if [ "$orientation" == "upside-down" ]; then
-            ./rotateimg tempstore/temp.$ext
-            mv tempstore/temp.$ext success/$(basename $f)
-        elif [ "$orientation" == "" ]; then
-            echo "Image size too small. Please try using an image of higher resolution."
-            cp $f failed/$(basename $f)
+            orientation=$(./detectflip tempstore/temp.$ext 2>&1)
+
+            if [ "$orientation" == "upside-down" ]; then
+                ./rotateimg tempstore/temp.$ext
+                mv tempstore/temp.$ext success/$(basename $f)
+            elif [ "$orientation" == "" ]; then
+                echo "Image size too small. Please try using an image of higher resolution."
+                cp $f failed/$(basename $f)
+            else
+                mv tempstore/temp.$ext success/$(basename $f)
+            fi
         else
-            mv tempstore/temp.$ext success/$(basename $f)
+            echo "Not an image. Moving on to the next file..."
         fi
-    else
-        echo "Not an image. Moving on to the next file..."
     fi
 done
 
 rm tempstore -rf
+echo "Done. Successful autorotations saved to "$rootdir"success and failures saved to "$rootdir"failed"
--- code/runQProcess/moc_tesqprocess.cpp
+++ code/runQProcess/moc_tesqprocess.cpp
@@ -1,8 +1,8 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'tesqprocess.h'
 **
-** Created: Thu Jul 11 18:20:47 2013
-**      by: The Qt Meta Object Compiler version 63 (Qt 4.8.1)
+** Created: Mon Jul 15 13:11:12 2013
+**      by: The Qt Meta Object Compiler version 63 (Qt 4.8.4)
 **
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
@@ -11,7 +11,7 @@
 #if !defined(Q_MOC_OUTPUT_REVISION)
 #error "The header file 'tesqprocess.h' doesn't include <QObject>."
 #elif Q_MOC_OUTPUT_REVISION != 63
-#error "This file was generated using the moc from 4.8.1. It"
+#error "This file was generated using the moc from 4.8.4. It"
 #error "cannot be used with the include files from this version of Qt."
 #error "(The moc has changed too much.)"
 #endif
@@ -23,7 +23,7 @@ static const uint qt_meta_data_tesQProcess[] = {
        6,       // revision
        0,       // classname
        0,    0, // classinfo
-       3,   14, // methods
+       4,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -34,6 +34,7 @@ static const uint qt_meta_data_tesQProcess[] = {
       13,   12,   12,   12, 0x08,
       28,   12,   12,   12, 0x08,
       43,   12,   12,   12, 0x08,
+      67,   12,   12,   12, 0x08,
 
        0        // eod
 };
@@ -41,6 +42,7 @@ static const uint qt_meta_data_tesQProcess[] = {
 static const char qt_meta_stringdata_tesQProcess[] = {
     "tesQProcess\0\0rightMessage()\0wrongMessage()\0"
     "on_btnProcess_clicked()\0"
+    "on_btnProcess2_clicked()\0"
 };
 
 void tesQProcess::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
@@ -52,6 +54,7 @@ void tesQProcess::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id,
         case 0: _t->rightMessage(); break;
         case 1: _t->wrongMessage(); break;
         case 2: _t->on_btnProcess_clicked(); break;
+        case 3: _t->on_btnProcess2_clicked(); break;
         default: ;
         }
     }
@@ -90,9 +93,9 @@ int tesQProcess::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 3)
+        if (_id < 4)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 3;
+        _id -= 4;
     }
     return _id;
 }
